5c91f14906f038b862fd1e14422ba5cbfcfb803a
Jepss06 <blackelimit@gmail.com>
2024-01-22 07:58:37 +0700

    test

---
+++ script/menu.js
@@ -0,0 +1,68 @@
+//isi nama variabel
+let  = [
+	{
+	  id: 0,
+	  name: "Paket 1",
+	  desc: "Nasi Timbel + Ayam (bakar/goreng) + Tahu & Tempe + Sambal + Teh",
+	  price: 10000,
+	}
+	{
+	  id: 1,
+	  name: "Paket 2",
+	  desc: "Nasi Timbel + Ayam (bakar/goreng) + Tahu & Tempe + Sambal + Teh",
+	  price: 20000,
+	}
+
+	{
+	  id: 2,
+	  name: "Paket 3",
+	  desc: "Nasi Timbel + Ayam (bakar/goreng) + Tahu & Tempe + Sambal + Teh",
+	  price: 30000,
+	},
+  ];
+  //perhatikan yang kurang
+  const menuSection = document.querySelector(".menu-section);
+  //maping variabel array object, panggil functionnya dan lengkapi value 
+  //yang dikirim ke function
+  //jangan lupakan paramter yang ada didalam mapping!
+  .map(() => {
+	menuSection.innerHTML += ( .desc,.name, .price, .id);
+  });
+  //lengkapi parameter function dan perhatikan penutup stringnya!
+  //tampilkan parameternya pada tag paragraf
+  //perhatikan kekurangan simbol untuk menampilkan parameter id
+  function iniApa() {
+	return `<div class="card">
+		  <button class="kategori" aria-readonly="true">Paket</button>
+		  <hr class="line" />
+		  <div class="container-content">
+			  <div>
+				  <img
+					  src="./images/example-product/indomie.jpg"
+					  alt=""
+					  class="produk-img"`
+				  />
+			  </div>
+			  <div class="content">
+				  <p></p>
+				  <p></p>
+				  <p></p>
+				  <div class="content2">
+					  <button class="tambah" id="pesan{id}">pesan</button>
+				  </div>
+			  </div>
+		  </div>
+	  </div>
+	  `;
+  }
+  //maping variabel array object, lalu perintahkan document untuk mengambil id pesan
+  //id pesan yang dipanggil disertai dengan id pada data didalam array object!
+  //jangan lupakan paramter yang ada didalam mapping!
+  //setelah id didapatkan, jalankan event onclick menggunakan addeventlistener
+  //lalu tampung prompt menggunakan var jumlah
+  //kalikan var jumlah dengan price
+  //tampung hasil perkalian kedalam localstorage
+  //redirect atau arahkan halaman pada order.html
+  .map(() => {
+
+  });
---
+++ script/script.js
@@ -0,0 +1,57 @@
+const formatUang = (rupiah) => {
+  return rupiah.toLocaleString("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  });
+};
+//ambil item nama
+let  = localStorage.("");
+//ambil item desc
+let  = localStorage.(")
+//perintahkan document untuk mengambil id info dan isi variabel untuk menampilkan nama dan desc!
+info = `anda telah memesan ${}: ${}`;
+//ambil item total
+let  = localStorage.("");
+//ubah element yang id nya total!
+//isi parseInt()!
+let  = (document.getElementById("total") = formatUang(
+  parseInt()
+  ));
+  let  = 50000;
+//ubah element yang id nya saldo!
+document.getElementById("saldo) = formatUang();
+const bayar = () => {
+  //mengambil resto-pay dan cash yang berbentuk input type radio, lalu ambil valuenya(true/false)
+  let  = document.getElementById("resto-pay");
+  let  = document.getElementById("cash");
+  // Cek memilih metode
+  if (!  !) {
+  
+  } else {
+    //check jika resto pay di pilih
+    if () {
+      //check jika saldo nya kurang!
+      if () {
+        //jika saldonya kurang maka akan terdapat pilihan apakah ingin topup atau engga
+         = ("Saldo Tidak Cukup ! apakah anda ingin topup?");
+        //jika pilihan tersebut di klik oke atau setuju untuk topup maka
+        if () {
+          //akan muncul isian atau formulir untuk topup!
+           = ("masukan nominal pecahan 10000");
+          //jika angka didalam var topup tersebut kelipatan sepuluh ribu maka
+          if () {
+           //saldo akan di tambahkan dengan topup!
+           //topup di konversi menjadi angka
+          }
+        }
+      }
+       
+      }
+    }
+  }
+} else if (cash != true) {
+};
+cashier.html";
+//else pertama masuk ke sukses.html dan else kedua masuk ke cashier
+cari penempatan else nya!
